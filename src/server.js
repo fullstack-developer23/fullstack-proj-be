cc0997aedd47bc03ce9b4dde55946d9e5570b756
NoraSaffar <norasaffar8910@gmail.com>
2024-01-24 14:35:03 +0000

    task: completed sign up and basic login in

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const userRouter = require("./user/routes");
 const port = process.env.PORT || 5001;
+
+const User = require("./user/model");
+const controllers = require("./user/controllers");
 
 const app = express();
 
@@ -10,10 +13,17 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use("/user", userRouter);
+
 app.get("/healthy", (req, res) => {
-    res.status(200).json({ message: "API is health"});
+  res.status(200).json({ message: "API is health" });
 });
 
-app.listen(port, () =>{
-    console.log(`App listening on port ${port}`);
+const syncTables = async () => {
+  await User.sync({ alter: true });
+};
+
+app.listen(port, () => {
+  syncTables();
+  console.log(`App listening on port ${port}`);
 });
---
+++ src/user/controllers.js
@@ -0,0 +1,28 @@
+const User = require("./model");
+
+const addUser = async (req, res) => {
+  try {
+    const user = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    res.status(201).json({ message: "User created", user: user });
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    res.status(201).json({ message: "User logged in", user: req.user });
+  } catch (error) {
+    res.status(500).json({ message: error.message, error: error });
+  }
+};
+
+module.exports = {
+  addUser: addUser,
+  login: login,
+};
